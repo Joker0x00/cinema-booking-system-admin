27201643301fc535de00ceadacaae844f951377f
wy <1811170468@qq.com>
2022-09-15 23:58:52 +0800

    前台用户订票基本功能实现

---
+++ src/views/front/select_movie/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>选择电影</h1>
+    <MovieList />
+  </div>
+</template>
+
+<script>
+import MovieList from '@/views/front/select_movie/MovieList/index.vue'
+export default {
+  name: 'SelectMovie',
+  components: {
+    MovieList
+  }
+}
+</script>
+
+<style>
+
+</style>
